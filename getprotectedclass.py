a5f4064df443ff45ace28ffbf383745c017d8efe
Matt <smithmatt53@gmail.com>
2023-11-23 11:44:02 -0600

    completed steps

--- getprotectedclass.py
+++ getprotectedclass.py
@@ -269,8 +269,8 @@ def ReadUpdatedFile():
 
 
 #Step 4 Divide Dataset
-def ReadAndSplitDataSet():
-    with open('updatedValues.csv') as file_obj: 
+def ReadAndSplitDataSet(fileName):
+    with open(fileName + '.csv') as file_obj: 
     # Create reader object by passing the file 
     # object to DictReader method 
         article_read = csv.DictReader(file_obj) 
@@ -323,7 +323,7 @@ def ReadTrainingTestingData(fileName):
                 if(x['City'] == 'Bangalore'):
                     score = score - 10
                 
-                if(x['EverBenched'] == 'Yes'):
+                if(x['EverBenched'] == '1'):
                     score = score - 20
                 scores.append({'Score': score,'Education': x['Education'], 'JoiningYear': x['JoiningYear'], 'City': x['City'], 'PaymentTier': x['PaymentTier'], 'Age': x['Age'], 'Gender': x['Gender'], 'EverBenched': x['EverBenched'], 'ExperienceInCurrentDomain': x['ExperienceInCurrentDomain'], 'LeaveOrNot': x['LeaveOrNot'], 'ID': x['ID']})
                 
@@ -338,6 +338,282 @@ def WriteTier3(name, values):
             writer.writerows(values)
 #updatedValues = ReadFile()
 #WriteFile('updatedValues',updatedValues)
+
+def FindTrainingResults(trainingResults):
+    falsePositive = []
+    falseNegative = []
+    right = []
+
+    for x in trainingResults:
+        if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
+            right.append(x)
+        elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
+            falseNegative.append(x)
+        elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
+            right.append(x)
+        elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
+            falsePositive.append(x)
+
+    print('accuracy', len(right))
+    print('false positive', len(falsePositive))
+    print('false negative', len(falseNegative))
+
+def FindTestingResults(testingResults):
+    falsePositiveTest = []
+    falseNegativeTest = []
+    rightTest = []
+    for x in testingResults:
+        if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
+            rightTest.append(x)
+        elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
+            falseNegativeTest.append(x)
+        elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
+            rightTest.append(x)
+        elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
+            falsePositiveTest.append(x)
+
+    print('accuracy',len(rightTest))
+    print('false positive',len(falsePositiveTest))
+    print('false negative', len(falseNegativeTest))
+
+    # get protected class percentage
+
+    malefalsePositiveTest = []
+    malefalseNegativeTest = []
+    malerightTest = []
+
+
+    femalefalsePositiveTest = []
+    femalefalseNegativeTest = []
+    femalerightTest = []
+    femaletherTest = []
+
+    falsePositiveTest40 = []
+    falseNegativeTest40 = []
+    rightTest40 = []
+    femaletherTest40 = []
+
+    falsePositiveTestnot40 = []
+    falseNegativeTestnot40 = []
+    rightTestnot40 = []
+    femaletherTestnot40 = []
+
+    maleScore45 = []
+    femaleScore45 = []
+    over40Score45 = []
+    under40Score45 = []
+
+    for x in testingResults:
+        if x['Gender'] == '1':
+            if int(x['Score']) >= 45:
+                maleScore45.append(x)
+            if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
+                malerightTest.append(x)
+            elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
+                malefalseNegativeTest.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
+                malerightTest.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
+                malefalsePositiveTest.append(x)
+        
+        elif x['Gender'] == '0':
+            if int(x['Score']) >= 45:
+                femaleScore45.append(x)
+            if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
+                femalerightTest.append(x)
+            elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
+                femalefalseNegativeTest.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
+                femalerightTest.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
+                femalefalsePositiveTest.append(x)
+
+        if x['Age'] == '1':
+            if int(x['Score']) >= 45:
+                over40Score45.append(x)
+            if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
+                rightTest40.append(x)
+            elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
+                falseNegativeTest40.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
+                rightTest40.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
+                falsePositiveTest40.append(x)
+
+        elif x['Age'] == '0':
+            if int(x['Score']) >= 45:
+                under40Score45.append(x)
+            if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
+                rightTestnot40.append(x)
+            elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
+                falseNegativeTestnot40.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
+                rightTestnot40.append(x)
+            elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
+                falsePositiveTestnot40.append(x)
+
+    print('male false positive:', len(malefalsePositiveTest))
+    print('male false negative:', len(malefalseNegativeTest))
+    print('male right:', len(malerightTest))
+
+    print('female false positive:', len(femalefalsePositiveTest))
+    print('female false negative:', len(femalefalseNegativeTest))
+    print('female right:', len(femalerightTest))
+    print(len(femaletherTest))
+
+    print('40 false positive:', len(falsePositiveTest40))
+    print('40 false negative:', len(falseNegativeTest40))
+    print('40 right:', len(rightTest40))
+    print(len(femaletherTest40))
+
+    print('not 40 false positive:', len(falsePositiveTestnot40))
+    print('not 40 false negative:', len(falseNegativeTestnot40))
+    print('not 40 right:', len(rightTestnot40))
+    print(len(femaletherTestnot40))
+
+    print('male score 45',len(maleScore45))
+    print('female score 45',len(femaleScore45))
+    print('over 40 score 45',len(over40Score45))
+    print('under 40 score 45',len(under40Score45))
+
+def NewFindTrainingResults(trainingResults):
+    falsePositive = []
+    falseNegative = []
+    right = []
+
+    for x in trainingResults:
+        if x['PaymentTier'] == '1' and int(x['Score']) >= 45:
+            right.append(x)
+        elif x['PaymentTier'] == '1' and int(x['Score']) < 45:
+            falseNegative.append(x)
+        elif x['PaymentTier'] == '0' and int(x['Score']) < 45:
+            right.append(x)
+        elif x['PaymentTier'] == '0' and int(x['Score']) >= 45:
+            falsePositive.append(x)
+
+    print('accuracy', len(right))
+    print('false positive', len(falsePositive))
+    print('false negative', len(falseNegative))
+
+def NewFindTestingResults(testingResults):
+    falsePositiveTest = []
+    falseNegativeTest = []
+    rightTest = []
+    for x in testingResults:
+        if x['PaymentTier'] == '1' and int(x['Score']) >= 45:
+            rightTest.append(x)
+        elif x['PaymentTier'] == '1' and int(x['Score']) < 45:
+            falseNegativeTest.append(x)
+        elif x['PaymentTier'] == '0' and int(x['Score']) < 45:
+            rightTest.append(x)
+        elif x['PaymentTier'] == '0' and int(x['Score']) >= 45:
+            falsePositiveTest.append(x)
+
+    print('accuracy',len(rightTest))
+    print('false positive',len(falsePositiveTest))
+    print('false negative', len(falseNegativeTest))
+
+    # get protected class percentage
+
+    malefalsePositiveTest = []
+    malefalseNegativeTest = []
+    malerightTest = []
+
+
+    femalefalsePositiveTest = []
+    femalefalseNegativeTest = []
+    femalerightTest = []
+    femaletherTest = []
+
+    falsePositiveTest40 = []
+    falseNegativeTest40 = []
+    rightTest40 = []
+    femaletherTest40 = []
+
+    falsePositiveTestnot40 = []
+    falseNegativeTestnot40 = []
+    rightTestnot40 = []
+    femaletherTestnot40 = []
+
+    maleScore45 = []
+    femaleScore45 = []
+    over40Score45 = []
+    under40Score45 = []
+
+    for x in testingResults:
+        if x['Gender'] == '1':
+            if int(x['Score']) >= 45:
+                maleScore45.append(x)
+            if x['PaymentTier'] == '1' and int(x['Score']) >= 45:
+                malerightTest.append(x)
+            elif x['PaymentTier'] == '1' and int(x['Score']) < 45:
+                malefalseNegativeTest.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) < 45:
+                malerightTest.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) >= 45:
+                malefalsePositiveTest.append(x)
+        
+        elif x['Gender'] == '0':
+            if int(x['Score']) >= 45:
+                femaleScore45.append(x)
+            if x['PaymentTier'] == '1' and int(x['Score']) >= 45:
+                femalerightTest.append(x)
+            elif x['PaymentTier'] == '1' and int(x['Score']) < 45:
+                femalefalseNegativeTest.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) < 45:
+                femalerightTest.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) >= 45:
+                femalefalsePositiveTest.append(x)
+
+        if x['Age'] == '1':
+            if int(x['Score']) >= 45:
+                under40Score45.append(x)
+            if x['PaymentTier'] == '1' and int(x['Score']) >= 45:
+                rightTestnot40.append(x)
+            elif x['PaymentTier'] == '1' and int(x['Score']) < 45:
+                falseNegativeTestnot40.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) < 45:
+                rightTestnot40.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) >= 45:
+                falsePositiveTestnot40.append(x)
+            
+
+        elif x['Age'] == '0':
+            if int(x['Score']) >= 45:
+                over40Score45.append(x)
+            if x['PaymentTier'] == '1' and int(x['Score']) >= 45:
+                rightTest40.append(x)
+            elif x['PaymentTier'] == '1' and int(x['Score']) < 45:
+                falseNegativeTest40.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) < 45:
+                rightTest40.append(x)
+            elif x['PaymentTier'] == '0' and int(x['Score']) >= 45:
+                falsePositiveTest40.append(x)
+
+    print('male false positive:', len(malefalsePositiveTest))
+    print('male false negative:', len(malefalseNegativeTest))
+    print('male right:', len(malerightTest))
+
+    print('female false positive:', len(femalefalsePositiveTest))
+    print('female false negative:', len(femalefalseNegativeTest))
+    print('female right:', len(femalerightTest))
+    print(len(femaletherTest))
+
+    print('40 false positive:', len(falsePositiveTest40))
+    print('40 false negative:', len(falseNegativeTest40))
+    print('40 right:', len(rightTest40))
+    print(len(femaletherTest40))
+
+    print('not 40 false positive:', len(falsePositiveTestnot40))
+    print('not 40 false negative:', len(falseNegativeTestnot40))
+    print('not 40 right:', len(rightTestnot40))
+    print(len(femaletherTestnot40))
+
+    print('male score 45',len(maleScore45))
+    print('female score 45',len(femaleScore45))
+    print('over 40 score 45',len(over40Score45))
+    print('under 40 score 45',len(under40Score45))
+
 
 """
 # Step 2
@@ -375,60 +651,36 @@ print('pay 3 under 40:' , len(vals[23]))
 """
 
 
-#step 4 - SPlit data sets
+#step 4 - SPlit Original data sets
 """
-train, test = ReadAndSplitDataSet()
+train, test = ReadAndSplitDataSet('updatedValues')
 WriteTrainingAndTestingFile('training', train)
 WriteTrainingAndTestingFile('testing', test)
 """
 
+#step 4 - Split threshold data sets
+"""
+train, test = ReadAndSplitDataSet('threshhold')
+WriteTrainingAndTestingFile('thresholdTraining', train)
+WriteTrainingAndTestingFile('thresholdTesting', test)
+"""
+
+"""
 trainingResults = ReadTrainingTestingData('training')
-#WriteTier3('trainingNot23', not3)
 WriteTier3('training23', trainingResults)
 testingResults = ReadTrainingTestingData('testing')
-#WriteTier3('testingNot23', not3)
 WriteTier3('testing23', testingResults)
 
+FindTrainingResults(trainingResults)
 
-falsePositive = []
-falseNegative = []
-right = []
-other = []
-for x in trainingResults:
-    if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
-        right.append(x)
-    elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
-        falseNegative.append(x)
-    elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
-        right.append(x)
-    elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
-        falsePositive.append(x)
-    else:
-        other.append(x)
+FindTestingResults(testingResults)
+"""
 
-print(len(right))
-print(len(falsePositive))
-print(len(falseNegative))
-print(len(other))
+thresholdTrainingResults = ReadTrainingTestingData('thresholdTraining')
 
+thresholdTestingResults = ReadTrainingTestingData('thresholdTesting')
 
-falsePositiveTest = []
-falseNegativeTest = []
-rightTest = []
-otherTest = []
-for x in testingResults:
-    if x['PaymentTier'] == '3' and int(x['Score']) >= 45:
-        rightTest.append(x)
-    elif x['PaymentTier'] == '3' and int(x['Score']) < 45:
-        falseNegativeTest.append(x)
-    elif x['PaymentTier'] != '3' and int(x['Score']) < 45:
-        rightTest.append(x)
-    elif x['PaymentTier'] != '3' and int(x['Score']) >= 45:
-        falsePositiveTest.append(x)
-    else:
-        other.append(x)
+NewFindTrainingResults(thresholdTrainingResults)
 
-print(len(rightTest))
-print(len(falsePositiveTest))
-print(len(falseNegativeTest))
-print(len(otherTest))
+NewFindTestingResults(thresholdTestingResults)
+
